6b5ce5ef19240a00836e4ef703da74e314583712
johnwang08 <johnwang08@hotmail.com>
2024-07-10 13:15:05 -0700

    added some customized text.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
        
-        Learn React
+        Learn React. This is react app it will be deployed automatically to MySWA via ci/cd.
    
       </header>
     </div>
