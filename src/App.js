f98e38631892fdb85c4f61975f7b30c676352597
John Wang <John.Wang@ledcor.com>
2024-08-11 19:27:25 -0700

    added Read Me

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         </p>
        
         This is react app it will be deployed automatically to MySWA via ci/cd.
+
+        <a href='https://ledcor.sharepoint.com/:w:/r/teams/IS-LTSCustomSolutions/Shared%20Documents/General/Enterprise%20Systems%20-%20LTS%20Solutions/Documentation/012%20Technical%20and%20Solution%20Architecture/Bell%20OPM/Solution%20Architecture%20Document%20-%20%5BBell%20OPM%20Workflow%20Automation%5D.docx?d=w2525ff52b9434faf82e56f77a3301af4&csf=1&web=1&e=JD67BS'>Resources Descripiton</a>
    
         <a href="/.auth/me">Me</a>&nbsp;|&nbsp;
         <a href="/.auth/logout">Log out</a>
